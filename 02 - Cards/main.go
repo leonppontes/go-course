92522432b2b461e5ab7a40256254f2aa06b4d699
unknown <leon.pontes@laceti.cin.ufpe.br>
2024-04-26 23:23:16 -0300

    first commit

---
+++ 02 - Cards/main.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+	cards := newDeck()
+	//cards.saveToFile("my_cards")
+	//cards := newDeckFromFile("my_cards")
+	cards.shuffle()
+	cards.print()
+
+	//hand, remainingDeck := deal(cards, 5)
+
+	//hand.print()
+	//remainingDeck.print()
+}
